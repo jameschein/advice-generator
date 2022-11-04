72403954fea6f670fd7b2f06b02cb253e7d76157
jamesschein <james@schein.co.uk>
2022-11-04 00:45:01 +0000

    ready for the API

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let advice = ['My advice to you at last in the day!','My advice to you Rudy.', 'Once upon a word of wisdom we flew high']
 
+    let randomNum = 0
+
     const die = document.querySelector('.cta-change-advice')
 
     const actualAdvice = document.querySelector('.actual-advice')
+
+    const adviceNum = document.querySelector('.advice-num')
 
     const generateNum = () => {
         return Math.random()
@@ -12,9 +16,19 @@ document.addEventListener('DOMContentLoaded', () => {
     } 
     
     die.addEventListener('click', (e)=>{
-        actualAdvice.innerHTML = advice[Math.floor(generateNum() * advice.length)]
+
+        randomNum = Math.floor(generateNum() * advice.length)
+        
+        actualAdvice.innerHTML = advice[randomNum]
+        adviceNum.innerHTML = randomNum + 1
     })
     
+    
+    randomNum = Math.floor(generateNum() * advice.length)
+        
+    actualAdvice.innerHTML = advice[randomNum]
+    adviceNum.innerHTML = randomNum + 1
+
     
 
 })
